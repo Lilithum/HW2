046f21d2881f64fd603120df3f9125d9c45e7d32
Lilithum <cengizmehmet95@hotmail.com>
2021-10-19 00:34:40 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,19 @@
+console.log("Merhaba Kodlama.io")
+
+
+var dolarBugun = 9.30
+
+console.log(dolarDun)
+var dolarDun = 9.20
+
+//array
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredileri"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
